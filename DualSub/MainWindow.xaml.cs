f43e8ef78e468c98db83c464465d23c0aac9ccce
DiepNV4 <diepnv4@fsoft.com.vn>
2019-06-12 18:44:59 +0700

    Fix Path

--- DualSub/MainWindow.xaml.cs
+++ DualSub/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using MahApps.Metro.Controls;
+﻿using CommonServiceLocator;
+using DualSub.ViewModel;
+using MahApps.Metro.Controls;
+using System.IO;
+using System.Linq;
+using System.Windows;
+
 namespace DualSub
 {
     public partial class MainWindow : MetroWindow
@@ -6,6 +12,18 @@ namespace DualSub
         public MainWindow()
         {
             InitializeComponent();
+
+            ServiceLocator.Current.GetInstance<MainViewModel>();
+        }
+
+        private void FilmFile_Drop(object sender, System.Windows.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                var file = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+                File.Copy(@"temp\converted.ass", Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file)) + ".ass");
+            }
         }
     }
 }
